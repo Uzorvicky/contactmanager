7f28661a7f4a16ec306ddb60e91ae3fab318b34a
Uzorvicky <uzorvicky@yahoo.com>
2020-01-14 13:47:32 +0100

    Updates

---
+++ precache-manifest.2ccd8ad8804d03f1f61f45950828c484.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67da132a7df463accb567117f7ccd9c",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "fb9fda5b443a3eb74eb2",
+    "url": "/contactmanager/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "41e1612ea44c75b5b1bb",
+    "url": "/contactmanager/static/css/main.cf08b432.chunk.css"
+  },
+  {
+    "revision": "fb9fda5b443a3eb74eb2",
+    "url": "/contactmanager/static/js/2.4ba29e5b.chunk.js"
+  },
+  {
+    "revision": "21294721c532847ffdd0953d624a66c8",
+    "url": "/contactmanager/static/js/2.4ba29e5b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41e1612ea44c75b5b1bb",
+    "url": "/contactmanager/static/js/main.785e7f02.chunk.js"
+  },
+  {
+    "revision": "168a4be25a5f18ccf386",
+    "url": "/contactmanager/static/js/runtime-main.a2b439a6.js"
+  }
+]);
